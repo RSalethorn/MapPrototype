db4509e719985044b1fff67b9da0f81aef011c4f
Rob <rob@email.com>
2023-03-21 20:27:49 +0000

    Changed from class to function component

--- App.js
+++ App.js
@@ -1,14 +1,7 @@
-class App extends React.Component {
-    constructor() {
-        this.state = {
-            message: "Hello Rob"
-        }
-    }
-    render(){
-        return(
-            <p>{this.state.message}</p>
-        );
-    }
+function App() {
+    const [message, setMessage] = UseState("Hello Rob");
+
+    return (<p>{message}</p>);
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
